9b216eaa5c78fb6886d176730a49a590206c3b60
hxed <121557650+Exo1i@users.noreply.github.com>
2025-03-23 09:34:45 +0200

    added DocumentController.java, SearchController.java

--- backend/src/main/java/cu/searchengine/Search/SearchController.java
+++ backend/src/main/java/cu/searchengine/Search/SearchController.java
@@ -1,26 +1,37 @@
 package cu.searchengine.Search;
 
 //import cu.searchengine.utils.Lemmatizer;
+
+import cu.searchengine.model.Documents;
 import cu.searchengine.utils.Tokenizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 @Controller
 @RestController
 @Component
 public class SearchController {
     private final Tokenizer tokenizer = new Tokenizer();
-//    private final Lemmatizer lemmatizer = new Lemmatizer();
+    //    private final Lemmatizer lemmatizer = new Lemmatizer();
     private final SearchService searchService = new SearchService();
     private final Logger logger = LoggerFactory.getLogger("SearchController");
 
-    @RequestMapping("/api/search")
-    public String search(@RequestParam String q) {
+//    public ResponseEntity<List<Documents>> search() {
+//        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+//    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam(required = false) String q, @RequestParam(required = false, defaultValue = "0") int page, @RequestParam(required = false, defaultValue = "10") int size) {
 
         return tokenizer.tokenize(q).toString();
 
