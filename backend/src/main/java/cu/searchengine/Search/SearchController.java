e7723b205a68d9724b90d2ba5490976500b54dab
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2025-04-17 15:10:30 +0200

    solve tokenizer edit..

--- backend/src/main/java/cu/searchengine/Search/SearchController.java
+++ backend/src/main/java/cu/searchengine/Search/SearchController.java
@@ -1,34 +1,34 @@
-package cu.searchengine.Search;
-
-//import cu.searchengine.utils.Lemmatizer;
-
-import cu.searchengine.utils.Tokenizer;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@Controller
-@RestController
-@Component
-public class SearchController {
-    private final Tokenizer tokenizer = new Tokenizer();
-    //    private final Lemmatizer lemmatizer = new Lemmatizer();
-    private final SearchService1 searchService1 = new SearchService1();
-    private final Logger logger = LoggerFactory.getLogger("SearchController");
-
-//    public ResponseEntity<List<Documents>> search() {
-//        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+//package cu.searchengine.Search;
+//
+////import cu.searchengine.utils.Lemmatizer;
+//
+//import cu.searchengine.utils.Tokenizer;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@Controller
+//@RestController
+//@Component
+//public class SearchController {
+//    private final Tokenizer tokenizer = new Tokenizer();
+//    //    private final Lemmatizer lemmatizer = new Lemmatizer();
+//    private final SearchService1 searchService1 = new SearchService1();
+//    private final Logger logger = LoggerFactory.getLogger("SearchController");
+//
+////    public ResponseEntity<List<Documents>> search() {
+////        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+////    }
+//
+//    @GetMapping("/search")
+//    public String search(@RequestParam(required = false) String q, @RequestParam(required = false, defaultValue = "0") int page, @RequestParam(required = false, defaultValue = "10") int size) {
+//
+//        return tokenizer.tokenizeWithPriority(q).toString();
+//
+////        return lemmatizer.lemmatize(q).toString();
 //    }
-
-    @GetMapping("/search")
-    public String search(@RequestParam(required = false) String q, @RequestParam(required = false, defaultValue = "0") int page, @RequestParam(required = false, defaultValue = "10") int size) {
-
-        return tokenizer.tokenize(q).toString();
-
-//        return lemmatizer.lemmatize(q).toString();
-    }
-}
+//}
